a8fa11ea0c06ac6c9fd84c6690b30517c7f4e3c3
Doxper <youngqi.jp@gmail.com>
2022-01-13 02:07:44 +0900

    ⚗️ 試しにクリックイベントを書いてみました

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,24 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
+import { useCallback } from "react";
 
 export default function Home() {
+  const handleClick = useCallback((e) => {
+    e.preventDefault();
+    alert(123);
+  }, []);
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
-
       </Head>
       <Header />
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
       <Main page="index" />
       <Footer />
     </div>
